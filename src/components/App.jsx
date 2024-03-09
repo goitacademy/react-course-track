e10a470896cac0c2f37417eac8b5adf1cf2d0d83
Alexander Repeta <alexander.repeta@gmail.com>
2024-03-09 22:55:40 +0200

    add lesson 7

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,23 @@
-import UserForm from "./UserForm";
+// import toast, { Toaster } from "react-hot-toast";
+// import ArticleList from "../ArticleList/ArticleList";
+// import SearchForm from "../SearchForm/SearchForm";
+// import { fetchArticles } from "../../article-api";
 
 export default function App() {
-  const addUser = (newUser) => {
-    console.log(newUser);
-  };
+  // const [searchQuery, setSearchQuery] = useState("");
+  // const [page, setPage] = useState(1);
+
+  // const [articles, setArticles] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(false);
 
   return (
     <div>
-      <h1>Forms with Formik</h1>
-      <UserForm onAdd={addUser} />
+      <button>Load more</button>
+
+      <p>
+        <b>Loading articles...</b>
+      </p>
     </div>
   );
 }
