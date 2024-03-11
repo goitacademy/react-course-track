96661556eca256a7ec8515e4aa2b9225fb562074
Alexander Repeta <alexander.repeta@gmail.com>
2024-03-11 19:39:02 +0200

    add lesson 9

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,9 @@
-// import UseMemoExample from "./UseMemoExample";
-// import RefBasics from "./RefBasics";
-// import RefExample from "./RefExample";
-// import TimerRef from "./TimerRef";
-// import LangSwitcher from "./LangSwitcher";
+import Layout from "./Layout/Layout";
 
 export default function App() {
   return (
-    <div>
-      {/* <UseMemoExample /> */}
-      {/* <RefBasics /> */}
-      {/* <RefExample /> */}
-      {/* <TimerRef /> */}
-
-      {/* <LangSwitcher /> */}
-      {/* <p>Selected lang: </p> */}
-    </div>
+    <Layout>
+      <h1>Routing in React</h1>
+    </Layout>
   );
 }
--- src/components/RefBasics.jsx
+++
@@ -1,9 +0,0 @@
-/**
- * - Створення та ініціалізація
- * - Життєвий цикл рефа
- * - Відсутність реактивності
- */
-
-export default function RefBasics() {
-  return <div>Ref basics</div>;
-}
