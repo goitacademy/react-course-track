8dba3353b8c62900b42e8f94c0189019c88f7388
Alexander Repeta <alexander.repeta@gmail.com>
2024-03-27 16:47:45 +0200

    remove next lesson code

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -11,7 +11,7 @@ export default function LoginForm() {
     <Formik
       initialValues={{
         email: "",
-        passsword: "",
+        password: "",
       }}
       onSubmit={handleSubmit}
     >
