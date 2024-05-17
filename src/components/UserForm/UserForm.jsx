d9df1c9edff093ad9cbcf0affd43ea9f2b0491fd
Alexander Repeta <alexander.repeta@gmail.com>
2024-05-17 22:05:32 +0300

    update examples

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,12 +1,14 @@
+import css from "./UserForm.module.css";
+
 export default function UserForm() {
   return (
-    <form>
-      <div>
+    <form className={css.form}>
+      <div className={css.group}>
         <label>Username:</label>
         <input type="text" name="username" />
       </div>
 
-      <div>
+      <div className={css.group}>
         <label>Role:</label>
         <select name="role">
           <option value="guest">Guest</option>
