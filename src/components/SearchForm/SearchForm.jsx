0e921fca0f409d84837654b64cb2047c75047ca9
Alexander Repeta <alexander.repeta@gmail.com>
2024-05-17 21:49:09 +0300

    update examples

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,16 +1,17 @@
 import { Field, Form, Formik } from "formik";
+// import css from "./SearchForm.module.css";
 
 export default function SearchForm({ onSearch }) {
   return (
     <Formik
-      initialValues={{ query: "" }}
+      initialValues={{ topic: "" }}
       onSubmit={(values, actions) => {
-        onSearch(values.query);
+        onSearch(values.topic);
         actions.resetForm();
       }}
     >
       <Form>
-        <Field type="text" name="query" />
+        <Field type="text" name="topic" />
         <button type="submit">Search</button>
       </Form>
     </Formik>
