651241252b119d37a2f9ae686604200d23c6d08c
Alexander Repeta <alexander.repeta@gmail.com>
2024-07-03 14:54:13 +0300

    Update UseMemoExample.jsx

--- src/components/UseMemoExample.jsx
+++ src/components/UseMemoExample.jsx
@@ -1,23 +1,73 @@
 import { useState } from "react";
 
 export default function UseMemoExample() {
-  const [a, setA] = useState(0);
-  const [b, setB] = useState(0);
-  const [c, setC] = useState(0);
+  const [users, setUsers] = useState([
+    { id: 1, name: "Jacob" },
+    { id: 2, name: "Lancer" },
+    { id: 3, name: "Meridian" },
+    { id: 4, name: "Kornwall" },
+    { id: 5, name: "Nathaniel" },
+  ]);
+  const [nameFilter, setNameFilter] = useState("");
 
-  console.log("Calculating value: a + b");
-  const value = a + b;
+  const addUser = () => {
+    setUsers((prevUsers) => [
+      ...prevUsers,
+      { id: crypto.randomUUID(), name: generateRandomName() },
+    ]);
+  };
 
-  // const memoizedValue = useMemo(() => {
-  //   console.log("Calculating memoizedValue: a + b");
-  // });
+  const changeFilter = (event) => {
+    setNameFilter(event.target.value);
+  };
+
+  console.log("Filtering users");
+  const filteredUsers = users.filter((user) =>
+    user.name.toLowerCase().includes(nameFilter.toLowerCase())
+  );
 
   return (
     <div>
-      <button onClick={() => setA(a + 1)}>Update a: {a}</button>
-      <button onClick={() => setB(b + 1)}>Update b: {b}</button>
-      <button onClick={() => setC(c + 1)}>Update c: {c}</button>
-      <p>value: {value}</p>
+      <button onClick={addUser}>Add random user</button>
+      <input type="text" value={nameFilter} onChange={changeFilter} />
+      <ul>
+        {filteredUsers.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateRandomName() {
+  const names = [
+    "John",
+    "Jane",
+    "Michael",
+    "Michelle",
+    "David",
+    "Emily",
+    "Chris",
+    "Ashley",
+    "James",
+    "Patricia",
+    "Robert",
+    "Linda",
+    "Mark",
+    "Barbara",
+    "Thomas",
+    "Elizabeth",
+    "Matthew",
+    "Jennifer",
+    "Anthony",
+    "Jessica",
+  ];
+
+  return names[getRandomInt(0, names.length - 1)];
 }
