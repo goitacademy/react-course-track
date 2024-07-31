360902f649282976f4afd29e7a80c9b14c892ade
Alexander Repeta <alexander.repeta@gmail.com>
2024-07-31 14:15:22 +0300

    Update slice.js

--- src/redux/tasks/slice.js
+++ src/redux/tasks/slice.js
@@ -1,5 +1,4 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { logOut } from "../auth/operations";
 import { fetchTasks, addTask, deleteTask } from "./operations";
 
 const tasksSlice = createSlice({
@@ -50,11 +49,6 @@ const tasksSlice = createSlice({
         state.isLoading = false;
         state.error = action.payload;
       })
-      .addCase(logOut.fulfilled, (state) => {
-        state.items = [];
-        state.error = null;
-        state.isLoading = false;
-      });
   },
 });
 
